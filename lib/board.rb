04e44ad3a9be9d6aa633f1d574cfe935c76defcf
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-03 12:24:32 -0800

    feat: Add display to Board

--- lib/board.rb
+++ lib/board.rb
@@ -5,4 +5,9 @@ class Board
     def initialize
         @board = Array.new(6){Array.new(7, '.')}
     end
+
+    def display
+        puts 'A B C D E F G'
+        @board.each {|row| puts row.join(' ')}
+    end
 end
