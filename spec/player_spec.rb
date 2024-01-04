3792ef5ae5840640a14d53de17226bf3ad209860
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-04 13:32:52 -0800

    test: Add additional tests for make_move to Player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -26,5 +26,36 @@ RSpec.describe Player do
 
             expect(board.cells.last[0]).to eq('X')
         end
+
+        it 'can make more than one move' do
+            board = Board.new
+            player = Player.new
+
+            player.make_move('A', board)
+            player.make_move('A', board)
+
+            expect(board.cells[-2][0]).to eq('X')
+        end
+
+        it 'returns nil if column is full' do
+            board = Board.new
+            player = Player.new
+
+            player.make_move('A', board)
+            player.make_move('A', board)
+            player.make_move('A', board)
+            player.make_move('A', board)
+            player.make_move('A', board)
+            player.make_move('A', board)
+
+            expect(player.make_move('A', board)).to eq(nil)
+        end
+
+        it 'returns nil if column is invalid' do
+            board = Board.new
+            player = Player.new
+
+            expect(player.make_move('H', board)).to eq(nil)
+        end
     end
 end
