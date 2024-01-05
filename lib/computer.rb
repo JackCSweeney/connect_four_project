ac8564c0ac49df10d49780de8daac06e690b31c5
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-04 19:14:52 -0500

    refactor: remove unecessary spaces

--- lib/computer.rb
+++ lib/computer.rb
@@ -53,19 +53,3 @@ class Computer
 
 
 end
-
-# method to scan the board for 3 in a row vertically, horizontally, and diagonally of it's own piece value and for the opponent piece value
-    # 
-
-    # columns.each do |column|
-    #     add_piece(column) if add_piece(column).win?
-
-    # columns = ('A'..'G').to_a
-    #     board_copy = board.clone
-    #     counter = 0
-    #     7.times do
-    #         board_copy.add_piece(columns[counter], 'X')
-    #         return columns[counter] if board_copy.win?
-    #         counter += 1
-    #     end
-    #     nil
