4ad263bd5e11899d81fbb699e6943c5572129168
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-05 15:58:41 -0800

    feat: Add play_game, take_turn, and main_menu, refactor endgame_win and endgame_draw in Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,7 @@
 require './lib/board'
+require './lib/player'
+require './lib/computer'
+
 class Game
 
     attr_reader :board, :player, :computer, :current_player, :turns_taken
@@ -11,8 +14,12 @@ class Game
         @turns_taken = 0
     end
 
+    def make_new_board
+        @board = Board.new
+    end
+
     def welcome_message
-        p 'Welcome to Connect Four'
+        puts 'Welcome to Connect Four'
     end
 
     def change_current_player
@@ -27,7 +34,6 @@ class Game
             puts 'Please select a valid column between A - G:'
             input = gets.chomp.upcase
         end
-        @player.make_move(input, @board)
     end
 
     def computer_turn
@@ -37,48 +43,62 @@ class Game
     end
 
     def endgame_win
-        if @board.win? && @current_player == @player
-            puts "You won!"
-        elsif @board.win? && @current_player == @computer
-            puts "You lose!"
-        end
+        @board.win?
     end
 
     def endgame_draw
-        if @board.draw?
-            puts "The game has ended in a draw!"
-        end
+        @board.draw?
     end
 
     def take_turn
-        endgame_win
-        endgame_draw
-        if @current_player == @player
-            player_turn
-        else
-            computer_turn
-        end
-        @turns_taken += 1
-        change_current_player
+        loop do
+                if @current_player == @player
+                    @board.display
+                    player_turn
+                else
+                    computer_turn
+                end
+                @turns_taken += 1
+                    if endgame_win && @current_player == @player
+                        puts "You won in a total of #{@turns_taken} turns!"
+                        @board.display
+                        break
+                    elsif endgame_win && @current_player == @computer
+                        puts "You lose!"
+                        @board.display
+                        break
+                    elsif endgame_draw
+                        puts "The game has ended in a draw!"
+                        break 
+                    end
+                change_current_player
+            end
+        play_game
     end
 
-            
+    def main_menu
+        puts "Press 'P' to play or 'Q' to quit"
+        input = gets.chomp.upcase
+    end
 
-        
-
+    def play_game
+        make_new_board
+        welcome_message
+        play_input = main_menu
+        loop do
+            case play_input
+                when 'P'
+                    take_turn
+                when 'Q'
+                    puts 'Goodbye!'
+                    break
+                else
+                    puts 'Please select a valid option'
+                    play_input = main_menu
+                end
+            end
+        end
 end
 
-# GAME FLOW
-# game should initialize with a computer and a player object
-# Welcome message and main menu where you select to play or quit
-# once play is selected, display game board and ask player to choose a column
-# will need to track current player through each turn, could be done by cycling through player and computer object, or use a counter
-# player chooses column, game board is updated with piece and displayed again
-    # can we delay this somehow so that there is a few seconds in between?
-    # if player selects invalid column (full or outside of range), it will ask to select a valid column and let them choose again until they choose a valid one
-# computer chooses column, game board is update with piece and displayed again
-# each turn will check for win? then check for draw? then ask to place piece
-    # this will happen before a turn is taken for both player and computer
-# once either win? or draw? happens, will display the correct message on whether the player won, lost, or the game was a draw
-# after the end game message is displayed, returns player to the main menu to ask to play again or quit
-
+game = Game.new
+game.play_game
